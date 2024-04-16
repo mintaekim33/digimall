d7ffdcb562307f793a4284a948318ad0ad3b2f6a
mintaekim <kmintae33@gmail.com>
2024-04-16 18:29:15 +0800

    navigate to payment url on checkout btn click

--- src/server.js
+++ src/server.js
@@ -18,16 +18,32 @@ const storeItems = new Map([
   [2, { priceInCents: 15000, name: "Learn CSS Today" }],
 ]);
 
-app.post("/create-checkout-session", (req, res) => {
-  // try {
-  //   // const session = await stripe.checkout.sessions.create({
-  //   //   payment_method_types
-  //   // });
-  //   // res.json({ url: session.url })
-  // } catch (e) {
-  //   res.status(500).json({ error: e.message });
-  // }
-  res.json({ url: "Hi" });
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map((item) => {
+        const storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.SERVER_URL}/sucess.html`,
+      cancel_url: `${process.env.SERVER_URL}/cancel.html`,
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+  // res.json({ url: "Hi" });
 });
 
 // Start up our server on port 3000
